664079ce5ef6613b726613c1ee8d56700e0bf587
MARUNGs <mi1764@naver.com>
2024-11-12 11:13:09 +0900

    error 설정

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,10 @@
+// 에러 복구전략
+"use client";
+
+export default function ErrorMsg() {
+  return (
+    <>
+      <h1>Error Message !</h1>
+    </>
+  );
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,11 @@ import { API_URL } from "../app/(home)/page";
 
 // api. 비디오 정보 호출
 async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  const json = await response.json();
-  return json;
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("오류가 발생했어요.");
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // const json = await response.json();
+  // return json;
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
