be4048a44c33825575eb2d74a3826516e1c22870
MARUNGs <mi1764@naver.com>
2024-11-12 17:22:11 +0900

    interface 중복 제거

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -23,10 +23,6 @@ export async function generateMetadata({ params: { id } }: IParams) {
   };
 }
 
-interface IParams {
-  id: string;
-}
-
 export default async function MovieDetail({ params: { id } }: IParams) {
   //여러 api를 호출하여 처리할 때, 한번에 처리하기 위해서는 Promise.all을 활용해보자.
   // Promise.all(): 여러 비동기 작업을 동시에 실행해준다. 완료될때까지 기다렸다가 한꺼번에 결과를 받는다.
